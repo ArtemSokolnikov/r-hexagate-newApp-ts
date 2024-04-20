f96c675bf393e3c85b7d269396edb07c66ce975b
Artem Sokolnikov <s9634331017@yandex.ru>
2024-04-20 13:00:33 +0300

    fixed commets

--- server/api/v1/funding/graph.py
+++ server/api/v1/funding/graph.py
@@ -14,11 +14,9 @@ def get_address_graph(
         address: str,
         chain: ChainId,
 ):
-    # Загрузить данные из JSON-файла
     with open("data.json", "r") as file:
         data = json.load(file)
 
-    # Фильтрация данных по переданному адресу
     graph_data = []
     for item in data:
         source_address = item["source"]["address"]
@@ -26,20 +24,17 @@ def get_address_graph(
         if source_address == address or dest_address == address:
             graph_data.append(item)
 
-    # Преобразование данных в структуру графа
     nodes = []
     edges = []
     for item in graph_data:
         source_address = item["source"]["address"]
         dest_address = item["dest"]["address"]
 
-        # Добавить узлы
         if source_address not in nodes:
             nodes.append(source_address)
         if dest_address not in nodes:
             nodes.append(dest_address)
 
-        # Добавить ребра
         edges.append(FundGraphEdge(source=source_address, target=dest_address))
 
     return FundGraphResponse(nodes=nodes, edges=edges)
